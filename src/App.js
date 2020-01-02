15e4ae733ebc5cc746779215bd5b002e384698e6
Dima Lis <readspore@gmail.com>
2020-01-02 12:08:07 +0200

    init

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>react-src/src/App.js</code> and save to reload.
+          Done
         </p>
         <a
           className="App-link"
